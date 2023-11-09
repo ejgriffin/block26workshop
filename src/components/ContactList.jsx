5d3897e58f41b1be34a7d388cae938c357070d75
Eric Griffin <ejgriffin13@gmail.com>
2023-11-09 17:49:35 -0500

    fetched data and useEffected all over the place

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import ContactRow from "./ContactRow";
 
@@ -8,6 +9,21 @@ const dummyContacts = [
 ];
 export default function ContactList() {
   const [contacts, setContacts] = useState(dummyContacts);
+
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          " https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
 
   return (
     <table>
