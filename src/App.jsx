db30c99e52fba20066ccee454860674a4b92a696
Eric Griffin <ejgriffin13@gmail.com>
2023-11-09 17:43:30 -0500

    table is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import ContactList from "./components/ContactList";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
 }
-
-export default App;
